983c872cd6626c3c1469447bfb318dfd430e4a11
Jordan James <jaj9608@mavs.uta.edu>
2023-06-14 22:31:39 -0500

    first

---
+++ app.py
@@ -0,0 +1,53 @@
+import math
+import pandas as pd
+import os, uuid
+from sqlalchemy import create_engine
+import pymysql
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
+app = Flask(__name__)
+
+server = 'q2server.mysql.database.azure.com'
+database = 'testdb'
+username = 'servadmin'
+password = '#hackme123'
+port = '3306'
+
+engine = create_engine(
+    f"mysql+pymysql://{username}:{password}@{server}:{port}/{database}",
+    connect_args={"ssl": {"ssl_ca": "DigiCertGlobalRootCA.crt.pem"}},
+)
+
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/range', methods=['POST'])
+def range():
+    
+    return render_template('range.html', ims = None)
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    v = None
+    
+    if v is not None:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = v)
+    else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+   app.run()
