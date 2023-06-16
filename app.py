61877d750d79b216bd0023a98f0572886fd64ba1
Jordan James <jaj9608@mavs.uta.edu>
2023-06-16 12:05:08 -0500

    update insert

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os, uuid
 from sqlalchemy import create_engine
 import pymysql
+from sqlalchemy import create_engine, update, MetaData, Table
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
@@ -98,9 +99,21 @@ def range():
     args[4] = float(args[4])
     df.loc[len(df.index)] = args
 
-    
-    df.to_sql('cities', con=engine, if_exists='replace',
-           index_label='idx')
+    # Define the data for the new record as a dictionary
+    new_record = {
+        'City': args[0],
+        'State': args[1],
+        'Population': args[2],
+        'lat': args[3],
+        'lon': args[4]
+    }
+
+    # Convert the dictionary into a DataFrame
+    ins = pd.DataFrame([new_record])
+
+    # Insert the record into the database
+    table_name = 'cities'
+    ins.to_sql(table_name, con=engine, if_exists='append', index=False)
     
     # Rendering the template with the query result
     if result is not None:
@@ -118,15 +131,29 @@ def increment():
     args[1] = int(args[1])
     args[2] = int(args[2])
     args[3] = int(args[3])
-    for i,r in enumerate(df["State"]):
-        if r == args[0] and int(df["Population"][i]) >= args[1] and int(df["Population"][i]) <= args[2]:
-            print(df["Population"][i])
-            df["Population"][i] = int(df["Population"][i])+  args[3]
-            print(df["Population"][i])
-            
+    query = '''SELECT *
+    FROM cities
+    WHERE Population BETWEEN {} AND {}
+    '''
     
-    df.to_sql('cities', con=engine, if_exists='replace',
-           index_label='idx')
+    # Table name, field to increment, and population range
+    metadata = MetaData(bind=engine)
+    table_name = 'cities'
+    table = Table(table_name, metadata, autoload=True)
+    field_to_increment = 'Population'
+    min_population = args[1]
+    max_population = args[2]
+
+    # Construct the update query
+    stmt = (
+        update(table)
+        .where(table.c.Population.between(min_population, max_population))
+        .values({field_to_increment: table.c.Population + args[3]})  
+    )
+
+    # Execute the update query
+    with engine.begin() as connection:
+        connection.execute(stmt)
     
     # Rendering the template with the query result
     if result is not None:
@@ -147,8 +174,13 @@ def count():
     arg = "'" + arg + "'"
     results = pd.read_sql_query(query.format(arg), engine)
     r = float(100)
-    f = float(results["lat"])
-    l = float(results["lon"])
+    print(results["lat"])
+    if len(results["lat"]) < 2:
+        f = float(results["lat"])
+        l = float(results["lon"])
+    else:
+        f = float(results["lat"][0])
+        l = float(results["lon"][0])
     
     ns = r/111        # North-south distance in degrees (69 for miles, 111 for km)
     ew = ns / math.cos(f) # East-west distance in degrees
